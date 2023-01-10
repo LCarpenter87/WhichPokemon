1216dcc46c70f37dab7bfb0b8d2aa27562c8e72f
LCarpenter87 <L.carpenter@outlook.com>
2023-01-10 20:35:30 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,21 @@ import cv2
 import numpy as np
 from keras.models import load_model
 from streamlit_webrtc import webrtc_streamer
+import av
 
-webrtc_streamer(key="example")
+
+flip = st.checkbox("Flip")
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:] if flip else img
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+
 
 #st.title('Is it Pikachu or Eevee!?')
 #st.header('The image detection tool you definitely do not need in your life')
