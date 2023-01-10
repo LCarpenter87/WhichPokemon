25fd408fa17b8f103b2779a31d9fc6d92b094aea
LCarpenter87 <L.carpenter@outlook.com>
2023-01-10 20:58:50 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
 
     flipped = img[::-1,:,:] if flip else img
+    
+    st.write('sup')
 
     return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
