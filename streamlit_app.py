d109c4534b0a20352cda2a1ba454561cef98116c
LCarpenter87 <L.carpenter@outlook.com>
2023-01-10 21:33:47 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,51 @@
 import streamlit as st
+import threading
 import cv2
 import numpy as np
 from keras.models import load_model
 from streamlit_webrtc import webrtc_streamer, RTCConfiguration, WebRtcMode
 import av
-
-
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
-flip = st.checkbox("Flip")
+model = load_model('keras_model.h5', compile = False)
+lock = threading.Lock() 
+img_container = {'img':None}
 
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
-
-    flipped = img[::-1,:,:] if flip else img
-    
-    st.write('sup')
-
-    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+    with lock:
+        img_container['img'] = img
+    return frame
 
 
-webrtc_streamer(key="example", 
+st.title('Is it Pikachu or Eevee!?')
+st.header('The image detection tool you definitely do not need in your life')
+
+ctx = webrtc_streamer(key="example", 
                 video_frame_callback=video_frame_callback,
                 rtc_configuration = RTC_CONFIGURATION,
                 mode=WebRtcMode.SENDRECV)
 
 
-#st.title('Is it Pikachu or Eevee!?')
-#st.header('The image detection tool you definitely do not need in your life')
+while ctx.state.playing:
+    with lock: 
+        img = img_container['img']
+    if img is None:
+        continue
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_AREA)
+    img = np.asarray(img, dtype=np.float32).reshape(1, 224, 224, 3)
+    img = (img / 127.5) - 1
+    probabilities = model.predict(image)
 
-#model = load_model('keras_model.h5', compile = False)
-#img_file_buffer = st.camera_input("Take a picture")
-
-
-#if img_file_buffer is not None:
-#    bytes_data = img_file_buffer.getvalue()
-#    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-#    image = cv2.resize(cv2_img, (224, 224), interpolation=cv2.INTER_AREA)
-#    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-#    image = (image / 127.5) - 1
-#    probabilities = model.predict(image)
-   
-
-#    if probabilities[0,0] > 0.8:
-#        prob = round(probabilities[0,0] * 100,2)
-#        st.write(f"I'm {prob}% sure that's Pikachu!")
-#        st.balloons()
-#    elif probabilities[0,1] > 0.8:
-#        prob = round(probabilities[0,1] * 100,2)
-#        st.write(f"I'm {prob}% pretty sure that's Eevee!")
-#        st.balloons()
-#    else:
-#        st.write("I have no idea what this is")
+    if probabilities[0,0] > 0.8:
+        prob = round(probabilities[0,0] * 100,2)
+        st.write(f"I'm {prob}% sure that's Pikachu!")
+        st.balloons()
+    elif probabilities[0,1] > 0.8:
+        prob = round(probabilities[0,1] * 100,2)
+        st.write(f"I'm {prob}% pretty sure that's Eevee!")
+        st.balloons()
+    else:
+        st.write("I have no idea what this is")
